45d6c600f548b18aaf19e8022158f10fa38aad36
Simon <simon.j.goring@gmail.com>
2025-10-07 14:06:28 -0700

    DS interval always excepts the last two days.

--- src/neotomadoi/sql/ds_timeslice.sql
+++ src/neotomadoi/sql/ds_timeslice.sql
@@ -4,4 +4,5 @@ LEFT JOIN ndb.datasetdoi AS dsdoi ON dsdoi.datasetid = ds.datasetid
 WHERE NOT ds.datasettypeid = 1
 AND dsdoi.doi IS NULL
 AND ds.recdatecreated > NOW() - INTERVAL %(interval)s
+AND ds.recdatecreated < NOW() - INTERVAL '2 DAYS'
 ORDER BY ds.datasetid;
